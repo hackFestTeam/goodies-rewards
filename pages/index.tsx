831f703169875d2a5dbadef6f5bf4298b52959e0
Bhavesh Mhadse <70565774+bhaveshmhadse@users.noreply.github.com>
2022-11-09 22:58:09 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Fashify - A Fashion tool to play with different types of outfits</title>
+        <title>Hackfest Designs - A Fashion tool to play with different types of outfits</title>
         <meta name='Hackfest Designs' content='A fashion tool to test out new outfits before going to shopping.' />
         <link rel='icon' href='/magic-wand.svg' />
       </Head>
